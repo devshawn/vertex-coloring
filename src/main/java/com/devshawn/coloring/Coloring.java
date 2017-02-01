e81a1858fcf5639d7b0904d240e05bd41a704a2d
Shawn Seymour <shawn@devshawn.com>
2017-02-01 15:23:58 -0600

    Implementing welsh powell heuristic

--- src/main/java/com/devshawn/coloring/Coloring.java
+++ src/main/java/com/devshawn/coloring/Coloring.java
@@ -1,6 +1,8 @@
 package com.devshawn.coloring;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Coloring {
 
@@ -17,6 +19,7 @@ public class Coloring {
                 result = applyGreedyHeuristic();
                 break;
             case WELSH_POWELL:
+                result = applyWelshPowell();
                 break;
             case MAXIMAL_INDEPENDENT_SET:
                 break;
@@ -26,6 +29,8 @@ public class Coloring {
     }
 
     // Implemented heuristics
+
+    // Greedy heuristic
     private ColoringResult applyGreedyHeuristic() {
 
         // Make result array of vertices, assign no color to them
@@ -64,6 +69,66 @@ public class Coloring {
 
         // Generate a ColorResult
         return new ColoringResult(result, ColoringHeuristic.GREEDY);
+    }
+
+    // Greedy plus degree sequencing heuristic (Welsh-Powell algorithm)
+    private ColoringResult applyWelshPowell() {
+
+        // Make result array of vertices, assign no color to them
+        int[] result = new int[graph.length];
+        for(int i = 0; i < graph.length; i++) result[i] = -1;
+
+        // Calculate degree of each vertex
+        int[] degrees = new int[graph.length];
+        Integer[] orderedVertices = new Integer[graph.length];
+
+        for(int i = 0; i < graph.length; i++) {
+            orderedVertices[i] = i;
+            for(int j = 0; j < graph.length; j++) {
+                if(graph[i][j] == 1) {
+                    degrees[i]++;
+                }
+            }
+        }
+
+        // Sort our vertices by decreasing degree
+        List<Integer> indexList = Arrays.asList(orderedVertices);
+        Collections.sort(indexList, (left, right) -> (degrees[indexList.indexOf(right)] - degrees[indexList.indexOf(left)]));
+        orderedVertices = indexList.toArray(new Integer[graph.length]);
+        System.out.println(Arrays.toString(orderedVertices));
+
+        // Make temporary available array to see what colors are available
+        boolean available[] = new boolean[graph.length];
+        for(int i = 0; i < graph.length; i++) available[i] = true;
+
+        // Color the graph!
+        for(int i = 0; i < graph.length; i++) {
+
+            // Check neighbors for colors
+            for(int j = 0; j < graph[orderedVertices[i]].length; j++) {
+                if(graph[orderedVertices[i]][j] == 1 && result[j] != -1) {
+                    available[result[j]] = false;
+                }
+            }
+
+            // Find smallest available color
+            int color = -1;
+            for(int j = 0; j < available.length; j++) {
+                if(available[j]) {
+                    color = j;
+                    break;
+                }
+            }
+
+            // Set this vertex to that color
+            result[orderedVertices[i]] = color;
+
+            // Reset our available colors
+            for(int j = 0; j < graph.length; j++) available[j] = true;
+        }
+
+
+        return new ColoringResult(result, ColoringHeuristic.WELSH_POWELL);
     }
 
     // Simple functions to get information about the graph
---
+++ src/test/java/com/devshawn/coloring/GreedySpec.java
@@ -0,0 +1,60 @@
+package com.devshawn.coloring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class GreedySpec {
+
+    @Test
+    public void smallGreedyCompleteGraph() {
+
+        // a complete 3-vertex graph
+        int[][] graph = {
+                {0, 1, 1},
+                {1, 0, 1},
+                {1, 1, 0}
+        };
+
+        Coloring coloring = new Coloring();
+        coloring.setGraph(graph);
+        ColoringResult result = coloring.applyHeuristic(ColoringHeuristic.GREEDY);
+
+        assertEquals(3, result.getColoringNumber());
+        int[] expectedColoring = {0, 1, 2};
+        assertArrayEquals(expectedColoring, result.getColoring());
+    }
+
+    @Test
+    public void smallGreedyGraph() {
+
+        // a 5-vertex graph
+
+        /*
+         * (0) ----- (2)
+         *  | \     / |
+         *  |   (4)   |
+         *  | /     \ |
+         * (1) ----- (3)
+         *
+         */
+
+        int[][] graph = {
+                {0, 1, 1, 0, 1},
+                {1, 0, 0, 1, 1},
+                {1, 0, 0, 1, 1},
+                {0, 1, 1, 0, 1},
+                {1, 1, 1, 1, 0}
+        };
+
+        Coloring coloring = new Coloring();
+        coloring.setGraph(graph);
+        ColoringResult result = coloring.applyHeuristic(ColoringHeuristic.GREEDY);
+
+        assertEquals(3, result.getColoringNumber());
+        int[] expectedColoring = {0, 1, 1, 0, 2};
+        assertArrayEquals(expectedColoring, result.getColoring());
+    }
+
+}
