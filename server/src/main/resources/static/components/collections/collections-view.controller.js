c2cadf19a016344ed7a9737b890b73bbfab7b686
Shawn Seymour <shawn@devshawn.com>
2017-04-03 22:37:49 -0500

    Updating collections

--- server/src/main/resources/static/components/collections/collections-view.controller.js
+++ server/src/main/resources/static/components/collections/collections-view.controller.js
@@ -7,6 +7,8 @@ angular.module('coloring')
         self.percentDifference = [];
         self.timePercentage = [];
         self.timePercentDifference = [];
+        self.minimumPercentDifference = [];
+        self.maximumPercentDifference = [];
 
         CollectionService.get({id: $stateParams.id}, function(results) {
             self.collection = results;
@@ -76,6 +78,72 @@ angular.module('coloring')
                 toPush.data = [greedywp, greedydsatur, wpdsatur];
 
                 self.percentDifference.push(toPush);
+            }
+
+            // minimum percent difference
+            for(var i = 0; i < self.collection.results.length; i++) {
+                var toPush = {
+                    name: self.collection.results[i].name,
+                    vertices: self.collection.results[i].vertices,
+                    data: [],
+                    series: ["GRDY-WP", "GRDY-DSAT", "WP-DSAT"],
+                    // series: ["GRDY-WP", "GRDY-MIS", "GRDY-DSAT", "WP-MIS", "WP-DSAT", "MIS-DSAT"],
+                    labels: []
+                }
+
+                var greedywp = [];
+                var greedymis = [];
+                var greedydsatur = [];
+                var wpmis = [];
+                var wpdsatur = [];
+                var misdsatur = [];
+
+                for(var j = 0; j < self.collection.results[i].comparisonSummaries.length; j++) {
+                    toPush.labels.push(self.collection.results[i].comparisonSummaries[j].name);
+                    greedywp.push(self.collection.results[i].comparisonSummaries[j].greedy_wp.minimumPercentDifference);
+                    greedymis.push(self.collection.results[i].comparisonSummaries[j].greedy_mis.minimumPercentDifference);
+                    greedydsatur.push(self.collection.results[i].comparisonSummaries[j].greedy_dsatur.minimumPercentDifference);
+                    wpmis.push(self.collection.results[i].comparisonSummaries[j].wp_mis.minimumPercentDifference);
+                    wpdsatur.push(self.collection.results[i].comparisonSummaries[j].wp_dsatur.minimumPercentDifference);
+                    misdsatur.push(self.collection.results[i].comparisonSummaries[j].mis_dsatur.minimumPercentDifference);
+                }
+
+                toPush.data = [greedywp, greedydsatur, wpdsatur];
+
+                self.minimumPercentDifference.push(toPush);
+            }
+
+            // maximum percent difference
+            for(var i = 0; i < self.collection.results.length; i++) {
+                var toPush = {
+                    name: self.collection.results[i].name,
+                    vertices: self.collection.results[i].vertices,
+                    data: [],
+                    series: ["GRDY-WP", "GRDY-DSAT", "WP-DSAT"],
+                    // series: ["GRDY-WP", "GRDY-MIS", "GRDY-DSAT", "WP-MIS", "WP-DSAT", "MIS-DSAT"],
+                    labels: []
+                }
+
+                var greedywp = [];
+                var greedymis = [];
+                var greedydsatur = [];
+                var wpmis = [];
+                var wpdsatur = [];
+                var misdsatur = [];
+
+                for(var j = 0; j < self.collection.results[i].comparisonSummaries.length; j++) {
+                    toPush.labels.push(self.collection.results[i].comparisonSummaries[j].name);
+                    greedywp.push(self.collection.results[i].comparisonSummaries[j].greedy_wp.maximumPercentDifference);
+                    greedymis.push(self.collection.results[i].comparisonSummaries[j].greedy_mis.maximumPercentDifference);
+                    greedydsatur.push(self.collection.results[i].comparisonSummaries[j].greedy_dsatur.maximumPercentDifference);
+                    wpmis.push(self.collection.results[i].comparisonSummaries[j].wp_mis.maximumPercentDifference);
+                    wpdsatur.push(self.collection.results[i].comparisonSummaries[j].wp_dsatur.maximumPercentDifference);
+                    misdsatur.push(self.collection.results[i].comparisonSummaries[j].mis_dsatur.maximumPercentDifference);
+                }
+
+                toPush.data = [greedywp, greedydsatur, wpdsatur];
+
+                self.maximumPercentDifference.push(toPush);
             }
 
             // time percent error
